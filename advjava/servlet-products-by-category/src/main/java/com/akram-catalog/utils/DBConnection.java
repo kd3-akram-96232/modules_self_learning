40011c2cd1efc0aaa290b0ec85391f1987daee5f
Akram Khan <khanakram8435@gmail.com>
2026-06-29 23:12:07 +0530

    UPDATE: modify database connection details in DBUtils class

--- advjava/servlet-products-by-category/src/main/java/com/akram-catalog/utils/DBConnection.java
+++ advjava/servlet-products-by-category/src/main/java/com/akram-catalog/utils/DBConnection.java
@@ -5,9 +5,9 @@ import java.sql.DriverManager;
 import java.sql.SQLException;
 
 public class DBUtils {
-	private final static String dbURL = "jdbc:mysql://localhost:3306/product_catalog";
-	private final static String userName = "root";
-	private final static String password = "manager";
+	private final static String dbURL = "jdbc:mysql://localhost:3306/servlet-products-by-category";
+	private final static String userName = "kd3_akram_96232";
+	private final static String password = "akram";
 	
 	public static Connection getConnection() throws SQLException
 	{
